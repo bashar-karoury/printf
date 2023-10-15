4ee6f3ed05c4a56c20ea337b2ec4fe313885f4d7
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-15 18:40:21 +0300

    Add printf.c

---
+++ printf.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _printf - function that print data to STDOUT according to format
+* @format: null terminated string that contains text to be printed
+* with specifers to format following provided arguments
+* @...: zero or more arguments to be printed to STDOUT, they should match
+* specifiers in format string
+*
+* Return: the number of characters printed excluding the null terminator
+* character, negative value if failed
+*/
+int _printf(const char *format, ...)
+{
+
+
+}
