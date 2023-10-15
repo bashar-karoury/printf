19f5f96d75a71f165bcc1489aaf6bb2bc122e456
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-15 18:21:53 +0000

    Update test files

--- test/test_printf.c
+++ test/test_printf.c
@@ -1,7 +1,10 @@
 #include "../main.h"
+#include <stdio.h>
 
 int main(void)
 {
-	_printf("bashar %i Sirelkhatim   %c", 11, 'A');
+	int n_c	= _printf("bashar %i Sirelkhatim   %c", 11, 'A');
+	printf("\n");
+	printf("Number of characters %d \n", n_c);
 	return (0);
 }
