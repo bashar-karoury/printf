fadbc4ad663ab59fd9c73ef03bcd1309f6f0fb7f
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-16 07:52:29 +0000

    Update declarations in main.h

--- main.h
+++ main.h
@@ -6,10 +6,10 @@
 /* Declarations */
 int _printf(const char *format, ...);
 int _putchar(char c);
-void print_c(char x);
-void print_string(char *str);
+int print_c(char x);
+int print_string(char *str);
 int print_modulus_mark(void);
-void print_Integer(int num);
+int print_Integer(int num);
 void printString(va_list ag, int *n_c);
 void printInt(va_list ag, int *p_n_c);
 void printChar(va_list ag, int *n_c);
