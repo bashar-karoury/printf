5a8d459d5d9a0b5700474f6013d2800d95b4fe94
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-16 11:08:52 +0000

    Update test_Integar

--- test/test_print_integer.c
+++ test/test_print_integer.c
@@ -3,15 +3,15 @@
 int main(void)
 {
 	int num;
-
+	
 	num = 484;
-	print_integer(num);
+	print_Integer(num);
 
 	num = -484;
-	print_integer(num);
+	print_Integer(num);
 
 	num = 38492;
-	print_integer(num);
+	print_Integer(num);
 
 	return (0);
 }
