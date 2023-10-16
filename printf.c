8281062d04915a12a20fe5980a618815ceabd924
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-16 11:08:22 +0000

    Update printf.c so it can handle special cases

--- printf.c
+++ printf.c
@@ -8,26 +8,9 @@ typedef struct{
 	printFunc function;
 }specifier_t;
 
-void printInt(va_list ag, int *p_n_c)
-{
-	printf("%i", va_arg(ag, int) );
-	(*p_n_c)++;
-}
-
-
-void printChar(va_list ag, int *p_n_c)
-{
-    printf("%c", (char )va_arg(ag, int));
-	(*p_n_c)++;
-}
-void printString(va_list ag, int *p_n_c)
-{
-    printf("%s", (char *)va_arg(ag, char *));
-	(*p_n_c)++;
-}
 
 static int print_until_specifier(const char **format, int *p_n_c);
-static void call_specifier_function(va_list ag, int *p_n_c, char sp);
+static void call_specifier_function(const char **format, va_list ag, int *p_n_c, char sp);
 /**
  * _printf - function that print data to STDOUT according to format
  * @format: null terminated string that contains text to be printed
@@ -46,23 +29,21 @@ int _printf(const char *format, ...)
 	va_start(ag, format);
 	while(print_until_specifier(&format, &(n_c)))
 	{
-		call_specifier_function(ag, &(n_c), *(++format));
-		format++;
+		call_specifier_function(&format, ag, &(n_c), *(++format));
 	}
 	return (n_c);
 }
 
-static void call_specifier_function(va_list ag, int *p_n_c, char sp)
+static void call_specifier_function(const char **format, va_list ag, int *p_n_c, char sp)
 {
 	int i = 0;
-
+	int specifier_found = 0;
 	const specifier_t specifiers[] = 
 	{
 		{'c', printChar		},
 		{'i', printInt		},
 		{'s', printString	},
 		{'d', printInt	   	},
-		{'%', printMod  	},
 		{0	,0				}
 	};
 
@@ -71,10 +52,24 @@ static void call_specifier_function(va_list ag, int *p_n_c, char sp)
 		if (sp == specifiers[i].symbol)
 		{
 			specifiers[i].function(ag, p_n_c);
+			specifier_found = 1;
 			break;
 		}
 		i++;
 	}
+	if(!(specifier_found))
+	{
+		if (sp == '%')
+		{
+			*(p_n_c) += print_c('%');
+		}
+		else
+		{
+			*(p_n_c) += print_c('%');
+			*(p_n_c) += print_c(**format);
+		}
+	}
+	 (*format)++;
 
 }
 
@@ -84,9 +79,8 @@ static int print_until_specifier(const char **format, int *p_n_c)
 		return 0;
 	while(**format != '%')
 	{
-		printf("%c", **format);
+		*(p_n_c) += print_c(**format);
 		(*format)++;
-		(*p_n_c)++;
 	}
 
 	return (1);
