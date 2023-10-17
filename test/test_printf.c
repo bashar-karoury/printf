b3012d3ee17f7bd7f12d52ac520ee2e27db92775
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-17 13:48:54 +0300

    Add test_printf_4

--- test/test_printf.c
+++ test/test_printf.c
@@ -5,8 +5,8 @@ int main(void)
 {
 	int n1,n2;
 
-	n1 = _printf("Negative:[%d]\n", -1);
-	n2 =  printf("Negative:[%d]\n", -1);
+	n1 = _printf("%             %", -1);
+	n2 =  printf("%             %", -1);
 
 	printf("n1 = %d, n2 = %d\n", n1, n2);
 	return (0);
